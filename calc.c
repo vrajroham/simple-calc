a69e91a257cdf577af9de0f819c931007e9b2426
Vaibhavraj Roham <vrajroham@users.noreply.github.com>
2016-09-01 22:53:20 +0530

    Created Project File
    created prototype

---
+++ calc.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main(){
+  int a,b,c;
+  c=add(a,b);
+  c=sub(a,b);
+  c=mul(a,b);
+}
+
+int add(int a ,int b){
+int c;
+//Logic
+return c;
+}
+
+int sub(int a ,int b){
+int c;
+//Logic
+return c;
+}
+
+int mul(int a ,int b){
+int c;
+//Logic
+return c;
+}
